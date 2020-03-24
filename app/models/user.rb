30497de0a193d9655671fa54a23d98141684ac20
github username <github email address>
2020-03-24 17:16:12 -0400

    finished auth logic before refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
     has_many :offers
     has_many :trades
     has_many :users, through: :trades
+    has_secure_password
+
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :name, presence: true
+    validates :password, presence: true
+    validates :password, length: { minimum: 6 }
+    validates :amount_of_corn, presence: true
+    validates :amount_of_corn, numericality: { only_integer: true, greater_than: 0}
+    # validates :amount_of_corn, numericality: { greater_than: 0}
+    validates :amount_of_money, presence: true
+    validates :amount_of_money, numericality: { only_integer: true, greater_than: 0}
+    # validates :amount_of_money, numericality: { greater_than: 0}
 end
